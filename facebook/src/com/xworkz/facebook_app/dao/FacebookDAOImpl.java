847233bf1e3c0c01374a6ff96e80e59f2e5b5449
snjku <snjkumarbr@gmail.com>
2021-05-31 15:01:29 +0530

    commit 31.5

--- facebook/src/com/xworkz/facebook_app/dao/FacebookDAOImpl.java
+++ facebook/src/com/xworkz/facebook_app/dao/FacebookDAOImpl.java
@@ -17,7 +17,7 @@ public class FacebookDAOImpl implements  FacebookDAO {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fbn", "root", "pasword");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fbn", "root", "passsword");
             statement = connection.createStatement();
             statement.execute("insert into facebook_table values(" + bookDTO.getId() + ",'" + bookDTO.getName() + "','" + bookDTO.getEmail() + "','" + bookDTO.getPassword() + "')");
 
